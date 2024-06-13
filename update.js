8c4c79f327580a3cd4e655d9ae9c6666cac240dd
Sameeksha Gupta <136142214+sameekshaagupta@users.noreply.github.com>
2024-06-13 16:32:00 +0530

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ const dbConnect= require('./mongodb')
 
 const updateData=async ()=>{
     let data = await dbConnect();
-    let result = await data.update(
+    let result = await data.updateOne(
         { name:'max 5'},
         {
             $set:{name:'max pro 5', price:1000}
