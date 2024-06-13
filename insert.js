d9af86e4a3eddbbd86065c5247be804db668c282
Sameeksha Gupta <136142214+sameekshaagupta@users.noreply.github.com>
2024-06-13 16:29:10 +0530

    Update insert.js

--- insert.js
+++ insert.js
@@ -1,19 +1,18 @@
-const dbConnect = require('./mongodb')
+const insertData=async ()=>{
+   let data = await dbConnect();
+  let result = await data.insertMany(
+      [
+          {name:'max 5',brand:'micromax',price:420,category:'mobile'},
+          {name:'max 6',brand:'micromax',price:520,category:'mobile'},
+          {name:'max 7',brand:'micromax',price:620,category:'mobile'},
 
-const insert = async () => {
-    let db =  await dbConnect();
-    let result = await db.insertMany(
-        [
-            {name:'moksha', rollno:'22BCP366', number:9087564734},
-            {name:'devam', rollno:'22BCP319', number:9087764734},
-            {name:'esha', rollno:'22BCP410', number:908004734},
-        ]
-    )
-    if(result.acknowledged){
-        console.log("Inserted successfully")
-    }
+      ]
+  )
+  if(result.acknowledged)
+  {
+      console.warn("data is inserted")
+  }
     else{
-        console.log("Not inserted")
+        console.log("data is not inserted")
     }
-}
-insert()
+insertData();
